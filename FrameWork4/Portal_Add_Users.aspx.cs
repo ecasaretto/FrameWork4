1d128f79d32f5c85e23c816606f924fc0b5c4143
PabloMorixe <45852748+PabloMorixe@users.noreply.github.com>
2020-05-12 19:40:18 -0300

    d

--- FrameWork4/Portal_Add_Users.aspx.cs
+++ FrameWork4/Portal_Add_Users.aspx.cs
@@ -14,6 +14,26 @@ namespace FrameWork4
 
         }
 
-      
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            var resultado = FrameWork4.AltaEmpleado.altaEmpleado(TxtNombre.Text.Trim(), TxtApellido.Text.Trim(), TxtCategoriaEmpleado.Text.Trim(),
+                   TxtEmpleadoEstado.Text.Trim(), CalendarFechaIngreso.SelectedDate, CalendarFechaNacimiento.SelectedDate,
+                   TxtCorreoElectronico.Text.Trim(), TxtTelefono.Text.Trim(), TxtNroCuil.Text.Trim(), TxtEmpleadoDomicilio.Text.Trim());
+            if (resultado.Item1 > 0)
+            {
+                Msg_AltaEmpleado.Text = "No se dio de alta al forro del empleado";
+                Msg_AltaEmpleado.Visible = true;
+
+
+                Response.Redirect("ReporteConsultoria.aspx");  // Acceso Concedido
+            }
+            else
+            {
+                Msg_AltaEmpleado.Text = "Usuario dado de alta correctamente ";
+                Msg_AltaEmpleado.Visible = true;
+
+            }
+        }
     }
 }
