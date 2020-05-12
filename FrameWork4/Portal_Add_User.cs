1a947d8515c5719f2fa6a4664fb789893d450130
PabloMorixe <45852748+PabloMorixe@users.noreply.github.com>
2020-05-12 19:34:03 -0300

    boton

---
+++ FrameWork4/Portal_Add_User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FrameWork4
+{
+    public class Portal_Add_User
+    {
+    }
+}
--- FrameWork4/Portal_Add_Users.aspx.designer.cs
+++ FrameWork4/Portal_Add_Users.aspx.designer.cs
@@ -78,6 +78,15 @@ namespace FrameWork4
         protected global::System.Web.UI.WebControls.CheckBox CheckBox1;
 
         /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
         /// Control Label1.
         /// </summary>
         /// <remarks>
