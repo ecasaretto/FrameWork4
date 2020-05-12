5f7f69a20d89f66c6a2b4d53e7d6519b54cd59f8
PabloMorixe <45852748+PabloMorixe@users.noreply.github.com>
2020-05-12 19:41:28 -0300

    d

--- FrameWork4/Portal_Add_User.cs
+++ FrameWork4/Portal_Add_User.cs
@@ -11,5 +11,37 @@ namespace FrameWork4
     }
 
 
+    internal static Tuple<int, string> altaEmpleado(string nombre, string Apellido, string EmpleadoCategoriaDescripcion,
+          string EmpleadoEstadoDescripcion, DateTime FechaAlta, DateTime FechaNacimiento, string CorreoElectronico,
+          string telefono, string NroCUIL, string Domicilio)
+    {
+        int res = 0;
+        string sessID = "";
+        DbCommand comm = Database.CreateCommand("sp_AltaEmpleado");
+        comm.Parameters.Add(Database.CreateParameter(comm, "@Nombre", DbType.String, nombre));
+        comm.Parameters.Add(Database.CreateParameter(comm, "@Apellido", DbType.String, Apellido));
+        comm.Parameters.Add(Database.CreateParameter(comm, "@EmpleadoCategoriaDescripcion", DbType.String, EmpleadoCategoriaDescripcion));
+        comm.Parameters.Add(Database.CreateParameter(comm, "@EmpleadoEstadoDescripcion", DbType.String, EmpleadoEstadoDescripcion));
+        comm.Parameters.Add(Database.CreateParameter(comm, "@FechaAlta", DbType.Date, FechaAlta));
+
+        comm.Parameters.Add(Database.CreateParameter(comm, "@FechaNacimiento", DbType.Date, FechaNacimiento));
+        comm.Parameters.Add(Database.CreateParameter(comm, "@CorreoElectronico", DbType.String, CorreoElectronico));
+        comm.Parameters.Add(Database.CreateParameter(comm, "@Telefono", DbType.String, telefono));
+
+        comm.Parameters.Add(Database.CreateParameter(comm, "@NroCUIL", DbType.String, NroCUIL));
+        comm.Parameters.Add(Database.CreateParameter(comm, "@Domicilio", DbType.String, Domicilio));
+
+        DataTable dt = Database.ExecuteSelectCommand(comm);
+        if (dt.Rows.Count > 0)
+        {
+            res = (int)dt.Rows[0]["res"];
+            sessID = dt.Rows[0]["sessionID"].ToString();
+
+        }
+        return Tuple.Create(res, sessID);
+
+    }
+
+
 
 }
